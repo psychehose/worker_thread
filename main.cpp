7cefd8193fc27451b194943aad39942e4470306f
psychehose <>
2025-06-12 17:57:45 +0900

    chore: remove unused

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
 #include "worker_thread_queue.h"
 #include <iostream>
 
-int sum(int a, int b) { return a + b; }
-
 int main(int argc, char *argv[]) {
   std::cout << "Hello, World!" << std::endl;
 
   ph::WorkerThreadQueue queue;
   queue.start();
-
-  queue.enqueue(std::bind(sum, 1, 2));
-
   queue.enqueue([]() { std::cout << "Hello, World2!" << std::endl; });
   queue.enqueue([]() { std::cout << "Hello, World3!" << std::endl; });
   queue.enqueue([]() { std::cout << "Hello, World4!" << std::endl; });
+  queue.stop();
 
   return 0;
 }
