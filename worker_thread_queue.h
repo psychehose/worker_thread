e910a1e3656a1c3ef441fe185af2be40b2bf42f9
psychehose <>
2025-06-13 16:43:18 +0900

    refactor: enqueue to support arbitary functions and argument with async  using template, future

--- worker_thread_queue.h
+++ worker_thread_queue.h
@@ -2,13 +2,17 @@
 
 #include <condition_variable>
 #include <functional>
+#include <future>
+#include <memory>
 #include <mutex>
 #include <queue>
 #include <thread>
+#include <type_traits>
 
 namespace ph {
-
 class WorkerThreadQueue {
+  template <typename Fn, typename... Args>
+  using Task = std::future<typename std::invoke_result<Fn, Args...>::type>;
 
 public:
   WorkerThreadQueue();
@@ -17,14 +21,28 @@ public:
   void start();
   void stop();
 
-  void enqueue(std::function<void()> task);
+  template <typename Fn, typename... Args>
+  auto enqueue(Fn &&fn, Args &&...args) -> Task<Fn, Args...> {
+
+    using return_type = typename std::invoke_result<Fn, Args...>::type;
+
+    auto task_to_enqueue = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...));
+
+    std::future<return_type> res = task_to_enqueue->get_future();
+    {
+      std::lock_guard<std::mutex> lock(queue_mutex_);
+      tasks_.push([task_to_enqueue]() { (*task_to_enqueue)(); });
+    }
+    cv_.notify_one();
+    return res;
+  }
 
 private:
   void workerThreadLoop();
 
 private:
   std::queue<std::function<void()>> tasks_;
-  std::thread worker_;
   std::mutex queue_mutex_;
   std::condition_variable cv_;
   std::thread worker_thread_;
